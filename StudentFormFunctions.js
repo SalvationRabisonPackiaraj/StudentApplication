336bd37f24783eb8ee29b1c51d84e312be0886b2
SalvationRabisonPackiaraj <101406033+SalvationRabisonPackiaraj@users.noreply.github.com>
2022-08-05 16:59:21 +0530

    front ui updated

--- StudentFormFunctions.js
+++ StudentFormFunctions.js
@@ -1,14 +1,16 @@
-
 //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
 //https://www.w3schools.com/jsref/event_onchange.asp
 //https://www.w3schools.com/jsref/event_preventdefault.asp
 //https://stackoverflow.com/questions/21814294/what-happens-when-submit-button-is-clicked
-const url ='http://localhost:8000/api/student';
+// const url ='http://localhost:8000/api/student';
+const url = "192.168.0.58:8080/api/rest"
 
-// Get Data from database
 let output = '';
 const postList = document.getElementById('table_body');
 
+const clearData = ()=> {
+    inputs.forEach(input => input.value = '');
+}
 const renderPosts = (posts)=>{
     posts.forEach(get =>{
         output +=`   
@@ -29,39 +31,79 @@ const renderPosts = (posts)=>{
     } );
     postList.innerHTML = output;
 }
-fetch(url + "/show")
+// Get Data from database
+
+fetch("http://192.168.0.126:8080/api/rest" + "/get")
  .then(res => res.json())
  .then(data =>renderPosts(data))
 
-
- // Post Data to Database
+ //Post Data to Database
  const addPostForm = document.querySelector('.add-post-form');
-
+ let inputs = document.querySelectorAll('input');
+// console.log(addPostForm)
  // Retrieve the data from form
 
-//  const nameData   = document.getElementById("name").value;
-//  const dobData  = document.getElementById("dob").value;
-//  const ageData   = document.getElementById("age").value;
-//  const joiningdatedData  = document.getElementById("joiningdate").value;
-//  const studentidData  = document.getElementById("studentid").value;
  
+     addPostForm.addEventListener('submit',(e) =>{
+       
+        e.preventDefault();
 
- addPostForm.addEventListener('submit',(e)=>{
-
-    e.preventDefault();
-
-    const payload = new FormData(addPostForm) ;
-
-    fetch(url + '/saving',{
+        
+ const nameData   = document.getElementById("exampleInputName1").value;
+ const dobData  = document.getElementById("exampleInputDob").value;
+ const ageData   = document.getElementById("disabledNumberInput").value;
+ const joiningdatedData  = document.getElementById("exampleInputJd").value;
+ const studentidData  = document.getElementById("exampleInputStudId").value;
+  
+ 
+    fetch("http://192.168.0.126:8080/api/rest" + "/save",{
         method: 'POST',
-        body : payload,
+        headers:{
+            'Content-Type' : 'application/json'
+        },
+        body : JSON.stringify({
+            name : nameData,
+            age : ageData,
+            dob : dobData,
+            jd : joiningdatedData,
+            studentId : studentidData
+        })
     })
     .then (res => res.json())
-    .then (data => 
+    .then (data =>
         {
         const dataArr = [];
         dataArr.push(data);
         renderPosts(dataArr);
+        clearData();
      })
 
 })
+
+
+///------------Find Age----------///
+
+
+function findAge(){
+    var day = document.getElementById("exampleInputDob").value;
+    var DOB = new Date(day);
+    var today = new Date();
+    var age = today.getTime()-DOB.getTime();
+    age = Math.floor(age/(1000*60*60*24*365.24));
+    document.getElementById("disabledNumberInput").value = age
+}
+
+
+////-----------swith ui--------------////
+
+
+
+function createClick() {
+    document.getElementById("formfield").style.display = "inline";
+    document.getElementById("tablefield").style.display = "none";
+}
+
+function listClick() {
+    document.getElementById("formfield").style.display = "none";
+    document.getElementById("tablefield").style.display = "inline";
+}
